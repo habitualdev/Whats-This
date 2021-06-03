7e630e8ebb03faa7ef27c6d487a968bdf0dd7f7b
habitual <habitual@Habitual-DT.habitual-media.com>
2021-06-03 15:35:51 -0400

    App Verify resolving changes

--- whats_this/bin/pywhat.py
+++ whats_this/bin/pywhat.py
@@ -11,16 +11,24 @@ n = 1
 class whatthis(StreamingCommand):
   def stream(self, records):
     for record in records:
-        full_string = ""
-        json_list = pyWhat.what_call.export_cli({record['_raw']})
+        message = ""
+        try:
+            message = self.fieldnames[0]
+        except:
+            message = '_raw'
+        json_list = pyWhat.what_call.export_cli({record[message]})
         json_str_list = (json.dumps(json_list)).split("},")
         record['what_raw'] = json_list
+        n = 1
         for json_str in json_str_list:
+            full_string = ""
             name_string = name_pull.findall(json_str)
             match_string = match_pull.findall(json_str)
             full_string = full_string + str(name_string) + ":" + str(match_string) +","
-        record["found_what"] =  full_string
+            record["found_what_" + str(n) ] =  full_string
+            n = n + 1
         yield record
+
 
 if __name__ == "__main__":
   dispatch(whatthis, sys.argv, sys.stdin, sys.stdout, __name__)
